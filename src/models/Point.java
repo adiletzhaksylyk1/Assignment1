c5cc5f91556ecdd9292c82c043d864b5f84321fa
adilet zhaksylyk <153743972+adiletzhaksylyk1@users.noreply.github.com>
2024-01-25 22:16:41 +0600

    2nd commit

--- src/models/Point.java
+++ src/models/Point.java
@@ -6,11 +6,22 @@ public class Point {
 
     // constructor having both parameters
     public Point(double x, double y) {
+        this.setX(x);
+        this.setY(y);
+    }
+    
+    public double setX() {
         this.x = x;
+    }
+    public double setY() {
         this.y = y;
     }
-
-
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
 
     // distance(Point dest) - distance from current point to the dest point
     public double distance(Point dest) {
